edc2a9d0287aeb7e0e8e6656c5dd54a972dd7143
Elif Tonbul <54376991+eliftnbl@users.noreply.github.com>
2021-01-31 15:35:58 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace AHPyöntemi_algoritma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Merkezi ",
+            "Kombi "});
+            this.comboBox2.Location = new System.Drawing.Point(271, 563);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 33;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 624);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 33);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Devam ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox6.Location = new System.Drawing.Point(222, 561);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(27, 25);
+            this.textBox6.TabIndex = 30;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox5.Location = new System.Drawing.Point(222, 517);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(27, 25);
+            this.textBox5.TabIndex = 29;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox4.Location = new System.Drawing.Point(222, 475);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(27, 25);
+            this.textBox4.TabIndex = 28;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(222, 434);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(27, 25);
+            this.textBox3.TabIndex = 27;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(222, 385);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 25);
+            this.textBox1.TabIndex = 25;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bookman Old Style", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(236, 24);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(265, 22);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Konut seçiminde kriterler";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(54, 481);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 19);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "M²-Oda sayısı ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(54, 514);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 19);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Bulunduğu kat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(54, 568);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 19);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Isınma Tipi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(54, 434);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 19);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Ulaşım-Sosyal alan      ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(54, 390);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 19);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Fiyat                           ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(20, 299);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(624, 40);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Aşağıda bulunan kriterleri sizin için önem sırasına göre sıralandırınız\r\n kriterl" +
+    "ere 1-5 arası numara veriniz ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(382, 488);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 36;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Konum ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 39;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(65, 145);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 17);
+            this.label10.TabIndex = 40;
+            this.label10.Text = "Çankaya";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(65, 182);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(64, 17);
+            this.label11.TabIndex = 41;
+            this.label11.Text = "Keçiören";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 22);
+            this.textBox2.TabIndex = 42;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(148, 182);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(27, 22);
+            this.textBox7.TabIndex = 43;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.Location = new System.Drawing.Point(44, 81);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(456, 18);
+            this.label12.TabIndex = 44;
+            this.label12.Text = "Aşağıda bulunan konumlara 1-2 arası önem sırası veriniz.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(929, 706);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AHPyöntemi_algoritma
+{
+    public partial class Form1 : Form
+    {
+        
+        Dictionary<int, string> konum = new Dictionary<int, string>();
+        Dictionary<int, string> kriter = new Dictionary<int, string>();
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+        int[] dizi = new int[5];
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int fiyat = Convert.ToInt32(textBox1.Text);
+            int ulaşım = Convert.ToInt32(textBox3.Text);
+            int oda = Convert.ToInt32(textBox4.Text);
+            int kat = Convert.ToInt32(textBox5.Text);
+            int ısıtma = Convert.ToInt32(textBox6.Text);
+            kriter.Add(fiyat, "fiyat");
+            kriter.Add(ulaşım, "ulaşım");
+            kriter.Add(ısıtma, "ısıtma");
+            kriter.Add(kat, "kat");
+            kriter.Add(oda, "oda");
+           
+            if (konum[1] == "çankaya")
+            {
+                for(int i = 1; i < 6; i++)
+                {
+                    if (kriter[i] == kriter[fiyat])
+                    {
+                        kriter[1] = 0.38.ToString();
+                    }
+                    else if (kriter[i] == kriter[ulaşım])
+                    {
+                        kriter[2] = 0.316.ToString();
+                    }
+                    else if (kriter[i] == kriter[oda])
+                    {
+                        kriter[3] = 0.38.ToString();
+                    }
+                    else if (kriter[i] == kriter[kat])
+                    {
+                        kriter[4] = 0.38.ToString();
+                    }
+                    else if (kriter[i] == kriter[ısıtma])
+                    {
+                        kriter[5] = 0.25.ToString();
+                    }
+
+                }
+               
+                if ( textBox1.Text == 1.ToString()) //BU KISIM FİYAT İÇİN
+                {
+                 fiyat = Convert.ToInt32(kriter[fiyat]);
+                 double önem1fiyat = fiyat * 0.4195;
+                }
+                else if (textBox1.Text == 2.ToString())
+                {
+                 fiyat = Convert.ToInt32(kriter[fiyat]);
+                 double önem2fiyat = fiyat * 0.216;
+                }
+                else if (textBox1.Text == 3.ToString())
+                {
+                    fiyat = Convert.ToInt32(kriter[fiyat]);
+                    double önem3fiyat = fiyat * 0.112;
+                }
+                else if (textBox1.Text == 4.ToString())
+                {
+                    fiyat = Convert.ToInt32(kriter[fiyat]);
+                    double önem4fiyat = fiyat * 0.056;
+                }
+                else if (textBox1.Text == 5.ToString())
+                {
+                    fiyat = Convert.ToInt32(kriter[fiyat]);
+                    double önem5fiyat = fiyat * 0.028;
+                }
+
+
+
+                if (textBox3.Text == 1.ToString()) //BU KISIM ULAŞIM İÇİN
+                {
+                    ulaşım = Convert.ToInt32(kriter[ulaşım]);
+                    double önem1ulaşım = ulaşım * 0.4195;
+                }
+                else if (textBox3.Text == 2.ToString())
+                {
+                    ulaşım = Convert.ToInt32(kriter[ulaşım]);
+                    double önem2ulaşım = ulaşım * 0.216;
+                }
+                else if (textBox3.Text == 3.ToString())
+                {
+                    ulaşım = Convert.ToInt32(kriter[ulaşım]);
+                    double önem3ulaşım = ulaşım * 0.112;
+                }
+                else if (textBox3.Text == 4.ToString())
+                {
+                    ulaşım = Convert.ToInt32(kriter[ulaşım]);
+                    double önem4ulaşım = ulaşım * 0.056;
+                }
+                else if (textBox3.Text == 5.ToString())
+                {
+                    ulaşım = Convert.ToInt32(kriter[ulaşım]);
+                    double önem5ulaşım = ulaşım * 0.0028;
+                }
+
+
+
+                if (textBox4.Text == 1.ToString()) //BU KISIM ODA İÇİN
+                {
+                    oda = Convert.ToInt32(kriter[oda]);
+                    double önem1oda = oda * 0.4195;
+                }
+                else if (textBox4.Text == 2.ToString())
+                {
+                    oda = Convert.ToInt32(kriter[oda]);
+                    double önem2oda = oda * 0.216;
+                }
+                else if (textBox4.Text == 3.ToString())
+                {
+                    oda = Convert.ToInt32(kriter[oda]);
+                    double önem3oda = oda * 0.112;
+                }
+                else if (textBox4.Text == 4.ToString())
+                {
+                    oda = Convert.ToInt32(kriter[oda]);
+                    double önem4oda = oda * 0.056;
+                }
+                else if (textBox4.Text == 5.ToString())
+                {
+                    oda = Convert.ToInt32(kriter[oda]);
+                    double önem5oda = oda * 0.028;
+                }
+
+
+                if (textBox5.Text == 1.ToString()) //BU KISIM KAT İÇİN
+                {
+                    kat = Convert.ToInt32(kriter[kat]);
+                    double önem1kat = oda * 0.4195;
+                }
+                else if (textBox5.Text == 2.ToString())
+                {
+                    kat = Convert.ToInt32(kriter[kat]);
+                    double önem2kat = oda * 0.216;
+                }
+                else if (textBox5.Text == 3.ToString())
+                {
+                    kat = Convert.ToInt32(kriter[kat]);
+                    double önem3kat=kat * 0.112;
+                }
+                else if (textBox5.Text == 4.ToString())
+                {
+                    oda = Convert.ToInt32(kriter[kat]);
+                    double önem4kat= kat * 0.056;
+                }
+                else if (textBox5.Text == 5.ToString())
+                {
+                    oda = Convert.ToInt32(kriter[kat]);
+                    double önem5kat = kat * 0.028;
+                }
+
+
+                if (textBox4.Text == 1.ToString()) //BU KISIM ISITMA İÇİN
+                {
+                    ısıtma = Convert.ToInt32(kriter[ısıtma]);
+                    double önem1ısıtma = ısıtma * 0.4195;
+                }
+                else if (textBox4.Text == 2.ToString())
+                {
+                    ısıtma = Convert.ToInt32(kriter[ısıtma]);
+                    double önem2ısıtma = ısıtma * 0.216;
+                }
+                else if (textBox4.Text == 3.ToString())
+                {
+                    ısıtma = Convert.ToInt32(kriter[ısıtma]);
+                    double önem3ısıtma = ısıtma * 0.112;
+                }
+                else if (textBox4.Text == 4.ToString())
+                {
+                    ısıtma = Convert.ToInt32(kriter[ısıtma]);
+                    double önem4ısıtma = ısıtma * 0.056;
+                }
+                else if (textBox4.Text == 5.ToString())
+                {
+                    ısıtma = Convert.ToInt32(kriter[ısıtma]);
+                    double önem5ısıtma = ısıtma * 0.028;
+                }
+
+
+
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            int çankaya = Convert.ToInt32(textBox2.Text);
+            int keçiören = Convert.ToInt32(textBox7.Text);
+            konum.Add(çankaya, "çankaya");
+            konum.Add(keçiören, "keçiören");
+            
+        }
+       
+        public void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
